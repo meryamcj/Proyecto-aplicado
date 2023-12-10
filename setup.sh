0a5b419753d53cc231c302eab352f70a58ceb004
Meryam Chaparro <alixcjimenez@gmail.com>
2023-12-09 21:14:43 -0500

    make it better 3

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# setup.sh
+
+# Create a virtual environment
+python -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
